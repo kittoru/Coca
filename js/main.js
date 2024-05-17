50e10b5317940c40dee1313a854d52abbfd44e31
Kittoru <gnetnevaee@gmail.com>
2024-05-17 18:34:37 +0300

    Add adaptive screens: 1200px, 1000px, 860px, 480px

--- js/main.js
+++ js/main.js
@@ -18,12 +18,27 @@ const insight_slider = new Swiper('.insight__slider', {
   },
 
   breakpoints: {
-    375: { 
+    1400: {
+      slidesPerView: 2,
+      spaceBetween: 32,
+    },
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1000: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    860: {
+      slidesPerView: 1.7,
+      spaceBetween: 20,
+    },
+    480: { 
       slidesPerView: 1.13,
       spaceBetween:16,
     }
   }
-
 });
 
 const testimonials_slider = new Swiper('.testimonials__main', {
